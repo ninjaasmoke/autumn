b9f77c5d740b079a36ea0b4009b1f862930cb694
Nithin Sai K J <nithins674@gmail.com>
2021-06-27 12:08:10 +0530

    added framer & nav

---
+++ components/Nav.tsx
@@ -0,0 +1,16 @@
+import navstyle from './Nav.module.css';
+import { motion } from 'framer-motion';
+
+const Nav: React.FC = () => {
+    return (
+        <motion.nav
+            className={navstyle.nav}
+            animate={{ top: 0, opacity: 1 }}
+            transition={{ delay: 0.2 }}
+        >
+            <h1>Autumn</h1>
+        </motion.nav>
+    )
+}
+
+export default Nav;
