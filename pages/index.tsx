3c0b4bd6a137e6163c12552a3573b2b55ccd917e
ninjaasmoke <nithins674@gmail.com>
2021-09-05 00:15:19 +0530

    added meta image

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,12 +8,12 @@ const Home: React.FC = () => {
     <div className={styles.container}>
       <Head>
         <title>autumn | code</title>
-        <meta name="description" content= "Code to image. Beautiful code, with Autumn Autumn is kind of an online code editor. Code & save images of the code or the code it self!" />
-        <meta name="robots" content= "index, follow" />
+        <meta name="description" content="Code to image. Beautiful code, with Autumn Autumn is kind of an online code editor. Code & save images of the code or the code it self!" />
+        <meta name="robots" content="index, follow" />
         <meta name="keywords" content="code, image, screenshot" />
         <meta name="theme-color" content="#000000" />
 
-        
+
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="author" content="ninjaasmoke" />
         <meta name="copyright" content="ninjaasmoke" />
@@ -21,6 +21,22 @@ const Home: React.FC = () => {
         <meta property="og:type" content="website" />
         <meta property="og:title" content="Autumn" />
         <link rel="icon" href="/favicon.ico" />
+
+        <meta name="title" content="autumn | code" />
+        <meta name="description" content="Code to image. Beautiful code, with Autumn Autumn is kind of an online code editor. Code & save images of the code or the code it self!" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://autumn.vercel.app/" />
+        <meta property="og:title" content="autumn | code" />
+        <meta property="og:description" content="Code to image. Beautiful code, with Autumn Autumn is kind of an online code editor. Code & save images of the code or the code it self!" />
+        <meta property="og:image" content="https://raw.githubusercontent.com/ninjaasmoke/autumn/main/autumn.png" />
+
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://autumn.vercel.app/" />
+        <meta property="twitter:title" content="autumn | code" />
+        <meta property="twitter:description" content="Code to image. Beautiful code, with Autumn Autumn is kind of an online code editor. Code & save images of the code or the code it self!" />
+        <meta property="twitter:image" content="https://raw.githubusercontent.com/ninjaasmoke/autumn/main/autumn.png" />
       </Head>
 
       {/* <Nav />
