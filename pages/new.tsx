b30e05ac27569b5b41477b35b36342c0c7b3ea64
ninjaasmoke <nithins674@gmail.com>
2021-09-04 17:07:33 +0530

    bg options

--- pages/new.tsx
+++ pages/new.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import { motion } from 'framer-motion';
@@ -20,33 +20,37 @@ import "ace-builds/src-noconflict/theme-tomorrow_night_eighties";
 import "ace-builds/src-noconflict/theme-gob";
 
 const bgs = {
-    blue: "linear-gradient(125deg, rgb(0, 0, 255), rgb(255, 0, 0))",
+    fireandice: "linear-gradient(-45deg, rgb(0, 0, 255), rgb(255, 0, 0))",
     candy: "linear-gradient(125deg, rgb(243, 8, 118), rgb(47, 0, 255))",
     candy2: "linear-gradient(125deg, #7303c0, #ec38bc)",
-    candy3: "linear-gradient(125deg, rgb(243, 8, 118), rgb(47, 0, 255))",
+    educate: "linear-gradient(125deg, #0d324d, #7f5a83)",
     pinotnoir: "linear-gradient(90deg,#4b6cb7,#182848)",
     plain: "linear-gradient(90deg, #141418, #141418)",
-    sopink: "linear-gradient(90deg,#ff0084,#33001b)",
     royal: "linear-gradient(90deg,#243B55,#141E30)",
+    sopink: "linear-gradient(90deg,#ff0084,#33001b)",
     teal: "linear-gradient(125deg, rgb(0, 194, 194), rgb(0, 50, 80))",
-    vanusa: "linear-gradient(125deg, #DA4453, #89216B)",
+    vanessa: "linear-gradient(125deg, #DA4453, #89216B)",
     witching: "linear-gradient(125deg, #c31432, #3d0000)",
-    wine: "linear-gradient(125deg,#b38ac4, #2e033d)",
+    wine: "linear-gradient(125deg,#b38ac4, #2e033d)", 
 }
+
+const bgNames = ["fireandice", "candy", "candy2", "educate", "pinotnoir", "plain", "royal", "sopink", "teal", "vanessa", "witching", "wine"];
 
 const New: React.FC = () => {
     const [openRightBar, setOpenRightBar] = useState<boolean>(true);
     const [edWidth, setEdWidth] = useState<number>(1200);
     const [edHeight, setEdHeight] = useState<number>(720);
 
-    const [backgroundTheme, setBackgroundTheme] = useState<string>(bgs.vanusa);
+    const [code, setCode] = useState<string>("");
+
+    const [backgroundTheme, setBackgroundTheme] = useState<string>(bgNames[Math.floor(Math.random() * bgNames.length)]);
     return (
         <div className={newStyle.new}>
             <Head>
                 <title>autumn | new</title>
             </Head>
 
-            <EditorElem edWidth={edWidth} edHeight={edHeight} background={backgroundTheme} />
+            <EditorElem edWidth={edWidth} edHeight={edHeight} background={backgroundTheme} code={code} setCode={setCode} />
 
             <motion.div
                 className={newStyle.rightBar}
@@ -58,9 +62,18 @@ const New: React.FC = () => {
                     className={newStyle.rEles}>
 
                     <div style={{
-                        display: openRightBar? "block" : "none",
+                        display: openRightBar ? "block" : "none",
                     }}>
                         <h4>Background</h4>
+                        {bgNames.map((bg, i) => {
+                            return (
+                                <div key={i} onClick={() => setBackgroundTheme(bg)} className={newStyle.bgOption} style={{
+                                    background: bgs[bg],
+                                }}>
+                                    {bg}
+                                </div>
+                            )
+                        })}
                     </div>
                 </div>
 
@@ -113,28 +126,39 @@ interface EditorProps {
     edWidth: number,
     edHeight: number,
     background: string,
- }
-const EditorElem: React.FC<EditorProps> = ({edWidth, edHeight, background}) => {
+    code: string,
+    setCode: React.Dispatch<React.SetStateAction<string>>,
+}
+const EditorElem: React.FC<EditorProps> = ({ edWidth, edHeight, background, code, setCode }) => {
     return (
-        <div className={newStyle.editorWrapper}
+        <div
+            id="editor"
+            className={newStyle.editorWrapper}
             style={{
                 width: edWidth, // 960 -> no padding
                 height: edHeight, // 492 -> no padding
-                background: background,
+                background: bgs[background],
             }}
         >
             <div className={newStyle.editorPadding}>
-               <div className={newStyle.topBar}>
-                   <div className={newStyle.topButtons}>
-                       <div className={newStyle.topBarButton} style={{backgroundColor: "rgb(196, 5, 5)"}} />
-                       <div className={newStyle.topBarButton} style={{backgroundColor: "rgb(255, 217, 0)"}} />
-                       <div className={newStyle.topBarButton} style={{backgroundColor: "rgb(22, 209, 84)"}} />
-                   </div>
-                   <input type="text" name="filename" id="filename" className={newStyle.filename} placeholder="Untitled" autoComplete="off" autoFocus={false} autoCorrect="off" />
-                   <div className={newStyle.topBuffer}></div>
-               </div>
+                <div className={newStyle.topBar}>
+                    <div className={newStyle.topButtons}>
+                        <div className={newStyle.topBarButton} style={{ backgroundColor: "rgb(196, 5, 5)" }} />
+                        <div className={newStyle.topBarButton} style={{ backgroundColor: "rgb(255, 217, 0)" }} />
+                        <div className={newStyle.topBarButton} style={{ backgroundColor: "rgb(22, 209, 84)" }} />
+                    </div>
+                    <input type="text" name="filename" id="filename" 
+                        className={newStyle.filename}
+                        placeholder="Untitled"
+                        autoComplete="off"
+                        autoFocus={false}
+                        spellCheck={false}
+                        autoCorrect="off"
+                        autoCapitalize="off" />
+                    <div className={newStyle.topBuffer}></div>
+                </div>
                 <AceEditor
-                    placeholder="Type something..."
+                    placeholder="Start typing..."
                     mode="javascript"
                     theme="nord_dark"
                     name="editor"
@@ -146,12 +170,11 @@ const EditorElem: React.FC<EditorProps> = ({edWidth, edHeight, background}) => {
                     }}
                     fontSize={14}
                     showPrintMargin={false}
-                    showGutter={false}                                    
+                    showGutter={false}
                     highlightActiveLine={false}
-                    value={`
-function hello() {
-    console.log("Hello World!");
-}`}
+                    wrapEnabled={true}
+                    onChange={(newCode) => setCode(newCode)}
+                    value={code}
                     setOptions={{
                         enableBasicAutocompletion: true,
                         enableLiveAutocompletion: true,
